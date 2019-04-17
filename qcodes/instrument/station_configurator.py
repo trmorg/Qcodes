d2b354e02c8b108794a2d0e865c828b181868236
Trevor Morgan (Prime 8 Consulting) <v-trmorg@microsoft.com>
2019-04-17 11:27:48 -0700

    Added context manager to handle strings and file-like objects for easier unit testing

--- qcodes/instrument/station_configurator.py
+++ qcodes/instrument/station_configurator.py
@@ -74,13 +74,12 @@ class StationConfigurator:
         if filename is None:
             filename = default_file
         try:
-            with open(filename, 'r') as f:
+            with open_filename(filename) as f:
                 self.config = yaml.load(f)
         except FileNotFoundError as e:
             if not os.path.isabs(filename) and default_folder is not None:
                 try:
-                    with open(os.path.join(default_folder, filename),
-                              'r') as f:
+                    with open_filename(os.path.join(default_folder, filename)) as f:
                         self.config = yaml.load(f)
                 except FileNotFoundError:
                     raise e
@@ -220,3 +219,24 @@ class StationConfigurator:
         Monitor(*self.monitor_parameters.values())
 
         return instr
+
+class open_filename(object):
+    """Context manager that opens a filename and closes it on exit, but does
+    nothing for file-like objects.
+    """
+    def __init__(self, filename, *args, **kwargs):
+        self.closing = kwargs.pop('closing', False)
+        if isinstance(filename, str):
+            self.fh = open(filename, *args, **kwargs)
+            self.closing = True
+        else:
+            self.fh = filename
+
+    def __enter__(self):
+        return self.fh
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.closing:
+            self.fh.close()
+
+        return False
